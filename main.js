0939543b6a371f4bdfae43774ede0a5f71bc9bdf
Terrance <Terrance.pickle@gmail.com>
2018-08-17 18:25:51 -0400

    AlmostFinilizedVer

---
+++ main.js
@@ -0,0 +1,15 @@
+function newPic0(){
+    document.getElementById("kodak").src="image/kodak-printer-paper.jpg";
+}
+
+function oldPic0(){
+    document.getElementById("kodak").src="image/kodak.jpg";   
+}
+
+function newPic2(){
+    document.getElementById("hp").src="image/hpPaper.jpg";
+}
+
+function oldPic2(){
+    document.getElementById("hp").src="image/emeahp.jpg";   
+}
